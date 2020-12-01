aba612d920b3d74ed4676feb3caf9702b4b6f107
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-12-01 18:06:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ function initMap() {
   var mischyLocation = new google.maps.LatLng(41.88480472032102, -87.62735095043277);
   var mapOptions = {
     center: mischyLocation,
-    //mapTypeId: google.maps.MapTypeId.HYBRID,
-    //mapTypeControlOptions: {
-      //position: google.maps.ControlPosition.RIGHT_CENTER
-    //}
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    }
   };
   
   // Marker

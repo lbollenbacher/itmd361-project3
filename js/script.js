0835db108f122ea8e96c9ecfd34f4b691348b488
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-12-01 18:04:04 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 // Map 
 
 function initMap() {
-  //alert('Welcome to Mischka's House!');
   var map = document.getElementById('canvas');
   var mischyLocation = new google.maps.LatLng(41.88480472032102, -87.62735095043277);
   var mapOptions = {
     center: mischyLocation,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    mapTypeControlOptions: {
-      position: google.maps.ControlPosition.RIGHT_CENTER
-    }
+    //mapTypeId: google.maps.MapTypeId.HYBRID,
+    //mapTypeControlOptions: {
+      //position: google.maps.ControlPosition.RIGHT_CENTER
+    //}
   };
   
   // Marker

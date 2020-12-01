c5557f1ecd3b1288ff4c02efeed833aa6fa93de4
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-12-01 16:06:45 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-// Map
+// Map variables
 
 var map;
+var mapCanvas;
+var mischyLocation;
+var mapOptions;
+var mischyMap;
+var marker;
+
+// Map 
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.88480472032102, lng: -87.62735095043277},
+  //alert('Welcome to Mischka's House!');
+  var mapCanvas = document.getElementById('canvas');
+  var mischyLocation = new google.maps.LatLng(41.88480472032102, -87.62735095043277);
+  var mapOptions = { 
+    center: mischyLocation,
     zoom: 15,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    }
+  };
+  
+  var mischyMap = new google.maps.Map(mapCanvas, mapOptions);
+  
+  var marker = new google.maps.Marker ({
+    position: mischyLocation,
+    map: mapCanvas,
+    animation: google.maps.Animation.DROP,
+    icon: ''
   });
-}
 
 google.maps.event.addEventListener(window, 'load', init);
+  
+  
+<script src="https://maps.googleapis.com/maps/api/js?key=  AIzaSyB4XAi_W8ZdN0xQt6aYzyF1oxwgSVDZB-s&callback=initMap" defer></script>

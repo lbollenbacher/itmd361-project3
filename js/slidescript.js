790a0f0a0331aa1cd8bf05129d0ca93d6e8d4fca
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-12-02 17:50:47 -0500

    Update slidescript.js

--- js/slidescript.js
+++ js/slidescript.js
@@ -1,30 +1,31 @@
 
 // Slideshow Variables
 
-var i = 0;
-var images = [];
-var time = 3000;
+  var i = 0;
+  var images = [];
 
 // Image List
 
-images[0] = "slideshow/birdwatching.jpeg";
-images[1] = "slideshow/dressup.jpeg";
-images[2] = "slideshow/hunting.jpeg";
-images[3] = "slideshow/munching.jpeg";
-images[4] = "slideshow/napping.jpeg"; 
-images[5] = "slideshow/playing.jpeg";
-images[6] = "slideshow/working.jpeg";
+  images[0] = "slideshow/birdwatching.jpeg";
+  images[1] = "slideshow/dressup.jpeg";
+  images[2] = "slideshow/hunting.jpeg";
+  images[3] = "slideshow/munching.jpeg";
+  images[4] = "slideshow/napping.jpeg"; 
+  images[5] = "slideshow/playing.jpeg";
+  images[6] = "slideshow/working.jpeg";
 
-// Function
+// Functions
 
 function changeImg() {
   document.getElementById("slide").src = images[i];
-
   if (i < images.length - 1) {i++;} 
   else {
     i = 0;
   }
-  setTimeout("changeImg()", time);
+}
+
+function slideShow() {
+  window.setInterval("changeImg()", 3000);
 }
 
 window.onload = changeImg;

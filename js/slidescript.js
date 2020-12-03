02ea1d9f4cbfc036028bf4888a5dec64096fae48
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-12-02 22:59:48 -0500

    Update slidescript.js

--- js/slidescript.js
+++ js/slidescript.js
@@ -1,31 +1,5 @@
-
-// Slideshow Variables
-
-  var i = 0;
-  var images = [];
-  var time = 3000;
-
-// Image List
-
-  images[0] = "slideshow/birdwatching.jpeg";
-  images[1] = "slideshow/dressup.jpeg";
-  images[2] = "slideshow/hunting.jpeg";
-  images[3] = "slideshow/munching.jpeg";
-  images[4] = "slideshow/napping.jpeg"; 
-  images[5] = "slideshow/playing.jpeg";
-  images[6] = "slideshow/working.jpeg";
-
-// Functions
-
-function changeImg() {
-  document.getElementById("slide").src = images[i];
-  if (i < images.length - 1) {i++;} 
-  else {
-    i = 0;
-  }
-  
-  setTimeout("changeImage()", time);
-}
-
-
-window.onload = changeImg;
+$(document).ready(function(){
+      $('.bxslider').bxSlider({
+        mode: 'fade'
+      });
+    });
